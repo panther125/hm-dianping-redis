75bb2261e4b2453ffece7871323e85d213082510
panther125 <2392189963@qq.com>
2023-05-08 16:25:05 +0800

    first commit

---
+++ src/main/java/com/panther/hmdp/HmDianPingApplication.java
@@ -0,0 +1,15 @@
+package com.panther.hmdp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.panther.hmdp.mapper")
+@SpringBootApplication
+public class HmDianPingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmDianPingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/panther/hmdp/dto/LoginFormDTO.java
@@ -0,0 +1,10 @@
+package com.panther.hmdp.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginFormDTO {
+    private String phone;
+    private String code;
+    private String password;
+}
